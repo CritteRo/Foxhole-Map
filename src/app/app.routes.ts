75ab70e6b2f6064265399dd108a5ddb4ce27b10d
rv-bit <robertvilcu89@gmail.com>
2025-08-08 13:51:50 +0100

    chore: add SSR test component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from "@angular/router";
+
 import { MapComponent } from "./map/map.component";
+import { TestComponent } from "./test/test.component";
 
 export const routes: Routes = [
   {
     path: "",
     component: MapComponent,
+  },
+  {
+    path: "test-ssr",
+    component: TestComponent,
   },
   {
     path: "**",
